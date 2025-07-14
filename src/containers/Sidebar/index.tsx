481bced96910e9fd816cffeff9df5eb1467d2733
MatheusMartinsN1 <mateusdecampos34@icloud.com>
2025-07-14 20:45:50 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
 import Titulo from '../../components/Titulo';
+import Avatar from '../../components/Avatar';
+import Paragrafo from '../../components/Paragrafo';
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles';
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/matheusmartinsn1.png" alt="Foto de perfil" />
-    <Titulo fontSize={20}>Matheus Martins</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Matheus Martins</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        MatheusMartinsN1
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 );
 
