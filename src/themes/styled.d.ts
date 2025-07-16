38649fcb24b3d40d200eb1a4ef75e560a2cb5acc
MatheusMartinsN1 <mateusdecampos34@icloud.com>
2025-07-16 20:26:51 -0300

    Projeto finalizado

---
+++ src/themes/styled.d.ts
@@ -0,0 +1,11 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    corPrincipal: string;
+    corSecundaria: string;
+    corDaBorda: string;
+    corDeFundo: string;
+    corDeFundoBotao: string;
+  }
+}
